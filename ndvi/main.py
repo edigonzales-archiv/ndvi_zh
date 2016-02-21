e984ab0b08548e0eb93da248785fbd64b736080b
Stefan Ziegler <edi.gonzales@gmail.com>
2016-02-21 19:48:13 +0100

    format

--- ndvi/main.py
+++ ndvi/main.py
@@ -9,7 +9,6 @@ from PyQt4.QtCore import *
 from qgis.core import *
 from qgis.analysis import QgsRasterCalculator, QgsRasterCalculatorEntry
 
-
 def main():
     # Read the options and arguments from the command line (w/ some default settings).
     options = Options()
@@ -19,9 +18,6 @@ def main():
     FORMAT = '%(asctime)s - %(levelname)s - %(message)s'
     logging.basicConfig(filename = opts.logfile, filemode = "w", format = FORMAT, level = logging.DEBUG)
     logging.getLogger().addHandler(logging.StreamHandler())
-
-
-
 
 if __name__ == '__main__':
     sys.exit(main())
